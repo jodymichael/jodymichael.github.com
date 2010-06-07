50846dcbb2480793f45a84c60610a721968a5efa
Dan Hodos <danhodos@gmail.com>
2010-06-07 15:55:03 -0500

    Site update

--- js/site.js
+++ js/site.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
- $.backstretch($(document.body).attr('data-background-image'));
+  $.backstretch($(document.body).attr('data-background-image'));
  
  $("#section-navigation-wrapper").tabs({ 
-  fx: { opacity: 'toggle' }, 
-  select: function(event, ui) {
-   $(ui.tab).blur();
-  }
+   fx: { opacity: 'toggle' }, 
+   select: function(event, ui) {
+     $(ui.tab).blur();
+   },
+   show: function(event, ui) {
+     $("#testimonial-list").css({ 'height': $("#testimonial-list li").height() });
+     $("#testimonial-list ul").cycle(0);
+   }
  });
  
  $(".contact article a, .media article a").each(function() {
-  $(this).attr('target', '_blank');
+   $(this).attr('target', '_blank');
  })
+ 
+ $("#testimonial-list ul").cycle({
+   prev:   '#prev', 
+   next:   '#next', 
+   timeout: 0,
+   nowrap: true,
+   // before: function(inElement, outElement, options) {
+   //   $("#next, #prev").removeClass('disabled');
+   // },
+   after: function(inElement, outElement, options) {
+     $("#next, #prev").removeClass('disabled');
+     if ($("#testimonial-list li").length-1 == options.currSlide) { $("#next").addClass('disabled'); }
+     if (0 == options.currSlide) { $("#prev").addClass('disabled'); }
+   }
+ }); 
 });
