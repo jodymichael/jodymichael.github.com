1911dbb151e5e4dd06af2c817ceb5d0011703c78
Dan Hodos <danhodos@gmail.com>
2010-06-14 17:09:26 -0500

    All updates for issues raised in Steve's email, including new IE6 support

--- js/site.js
+++ js/site.js
@@ -1,33 +1,36 @@
 $(document).ready(function() {
   $.backstretch($(document.body).attr('data-background-image'));
- 
- $("#section-navigation-wrapper").tabs({ 
-   fx: { opacity: 'toggle' }, 
-   select: function(event, ui) {
-     $(ui.tab).blur();
-   },
-   show: function(event, ui) {
-     $("#testimonial-list").css({ 'height': $("#testimonial-list li").height() });
-     $("#testimonial-list ul").cycle(0);
-   }
- });
- 
- $(".contact article a, .media article a").each(function() {
-   $(this).attr('target', '_blank');
- })
- 
- $("#testimonial-list ul").cycle({
-   prev:   '#prev', 
-   next:   '#next', 
-   timeout: 0,
-   nowrap: true,
-   // before: function(inElement, outElement, options) {
-   //   $("#next, #prev").removeClass('disabled');
-   // },
-   after: function(inElement, outElement, options) {
-     $("#next, #prev").removeClass('disabled');
-     if ($("#testimonial-list li").length-1 == options.currSlide) { $("#next").addClass('disabled'); }
-     if (0 == options.currSlide) { $("#prev").addClass('disabled'); }
-   }
- }); 
+
+  $("#testimonial-list li").each(function() {
+    $(this).html($(this).html().replace(/<br\/?>/gi,'<div class="spacer">&nbsp;</div>')); 
+  });
+  
+  $("#section-navigation-wrapper").tabs({ 
+    fx: { opacity: 'toggle' }, 
+    select: function(event, ui) {
+      $(ui.tab).blur();
+    },
+    show: function(event, ui) {
+      $("#testimonial-list").css({ 'height': $("#testimonial-list li").height() });
+      $("#testimonial-list ul").cycle(0);
+    }
+  });
+  
+  $(".contact article a, .media article a").each(function() {
+    $(this).attr('target', '_blank');
+  })
+    
+  $("#testimonial-list ul").cycle({
+    prev:   '#prev', 
+    next:   '#next', 
+    timeout: 0,
+    nowrap: true,
+    cleartype:  true,
+    cleartypeNoBg:  true,
+    after: function(inElement, outElement, options) {
+      $("#next, #prev").removeClass('disabled');
+      if ($("#testimonial-list li").length-1 == options.currSlide) { $("#next").addClass('disabled'); }
+      if (0 == options.currSlide) { $("#prev").addClass('disabled'); }
+    }
+  }); 
 });
